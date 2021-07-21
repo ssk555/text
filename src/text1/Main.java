04c1d4acac57adf1dcba6bef3442f9af5d4c8d97
ssk555 <c513984369@gmail.com>
2021-07-21 23:33:16 +0800

    Initial commit

---
+++ src/text1/Main.java
@@ -0,0 +1,9 @@
+package text1;
+
+public class Main {
+    public static void main(String[] args) {
+        mian niu=new mian();
+        System.out.println("public:"+niu.vcc1());
+
+    }
+}
---
+++ src/text1/mian.java
@@ -0,0 +1,23 @@
+package text1;
+
+import java.util.Scanner;
+
+public class mian {
+    public int vcc1()
+    {
+        return 99;
+    }
+    public mian() {
+    }
+
+    public static void main(String[] args) {
+        int text=99;
+        System.out.println("hello world"+text);
+       System.out.println(text);
+       int[] mm=new int[text];
+        Scanner in=new Scanner(System.in);
+       mm[0]=in.nextInt();
+       System.out.println(mm[0]);
+
+    }
+}
